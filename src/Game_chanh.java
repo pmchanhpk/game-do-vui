af979e57f6d9afcd45ac1b5214f75db6a9e4525d
pmchanhpk <pmchanhpk@gmail.com>
2017-09-06 18:34:55 +0700

    update image

--- src/Game_chanh.java
+++ src/Game_chanh.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Paths;
-import java.text.Collator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
